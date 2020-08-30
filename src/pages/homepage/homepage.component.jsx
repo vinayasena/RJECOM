7cac607a299d9047d3c2295f5c7a009f83acf092
unknown <vinayasenareddy.k@gmail.com>
2020-08-30 22:56:09 +0530

    implemented the home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/ecom-directory/directory.component';
+
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory/>
+        </div> 
+
+    )
+}
+export default Homepage; 
